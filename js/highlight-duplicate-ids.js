384c5f1eb062182e31f9c748d4210a451fe4f8ff
Hugo Dzin <humodz065@gmail.com>
2022-08-23 10:23:05 -0300

    fix: highlight-duplicate-ids

--- js/highlight-duplicate-ids.js
+++ js/highlight-duplicate-ids.js
@@ -2,8 +2,8 @@ javascript:
 (() => {
   const elements = [...document.querySelectorAll('[id]')];
   const ids = elements.map(el => el.id);
-  const dups = elements.filter(el => ids.filter(id => id === el.id).length > 1);
-  dups.forEach(el => el.style.border = '1px solid red !important');
+  const dups = elements.filter(el => ids.includes(el.id));
+  dups.forEach(el => el.style.border = '1px solid red');
 
   dups.sort((el1, el2) => el1.id.localeCompare(el2.id));
   console.log(dups);
