77225c792928d5053069f681313c16661a8d4e97
Hugo Dzin <humodz065@gmail.com>
2022-08-23 10:29:15 -0300

    fix: highlight-duplicate-ids

--- js/highlight-duplicate-ids.js
+++ js/highlight-duplicate-ids.js
@@ -2,7 +2,7 @@ javascript:
 (() => {
   const elements = [...document.querySelectorAll('[id]')];
   const ids = elements.map(el => el.id);
-  const dups = elements.filter(el => ids.includes(el.id));
+  const dups = elements.filter(el => ids.filter(id => id === el.id).length > 1);
   dups.forEach(el => el.style.border = '1px solid red');
 
   dups.sort((el1, el2) => el1.id.localeCompare(el2.id));
--- js/show-passwords.js
+++ js/show-passwords.js
@@ -1,6 +1,15 @@
 javascript:
 (() => {
-  document
-    .querySelectorAll('[type="password"]')
-    .forEach(e => e.type = 'text');
+  const passwordFields = document.querySelectorAll('[type="password"]');
+  const exposedPasswordfields = document.querySelectorAll('[data-was-password]');
+
+  passwordFields.forEach(e => {
+    e.type = 'text';
+    e.setAttribute('data-was-password', '');
+  });
+
+  exposedPasswordfields.forEach(e => {
+    e.type = 'password';
+    e.removeAttribute('data-was-password');
+  });
 })()
