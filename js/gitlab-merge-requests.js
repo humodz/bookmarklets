3a4bcc8c9c9588e030732502d6ca14e694c00349
Hugo Dzin <humodz065@gmail.com>
2022-08-23 15:39:43 -0300

    wip

--- js/gitlab-merge-requests.js
+++ js/gitlab-merge-requests.js
@@ -5,15 +5,14 @@ javascript:
     return;
   }
 
-	/* Notification.requestPermission(); */
+	Notification.requestPermission();
 
 	const $ = (s, e = document) => e.querySelector(s);
 	const $$ = (s, e = document) => [...e.querySelectorAll(s)];
 	const parser = new DOMParser();
 
 	async function fetchDom(url) {
-		const response = await fetch(url);
-		const html = await response.text();
+		const html = await fetch(url).then(res => res.text());
 		return parser.parseFromString(html, 'text/html');
 	}
 
@@ -35,7 +34,9 @@ javascript:
 		return projects;
 	}
 
-	async function updateMergeRequestList() {
+	let oldMrInfos = [];
+
+	async function updateMergeRequestList(first = false) {
 		console.log('Updating merge requests...', new Date());
 		const urls = (await fetchStarred()).map(it => it + '/-/merge_requests');
 
@@ -44,9 +45,8 @@ javascript:
 		);
 
 		const contentBody = $('#content-body');
-		$$('.issues-filters ~ *', contentBody).map(it => it.remove());
 
-		const mrInfos = [];
+		$$('#content-body > *').forEach(it => it.remove());
 
 		for (const [url, mrs] of mrsByProject) {
 			if (mrs) {
@@ -56,6 +56,28 @@ javascript:
 				contentBody.append(br, title, mrs);
 			}
 		}
+
+		const newMrInfos = getMergeRequestInfos();
+		console.log('!!!', newMrInfos);
+
+		if (!first) {
+			const myself = await getCurrentUser();
+			const changes = diff(oldMrInfos, newMrInfos);
+
+			const interestingChanges = changes.filter(
+				change => !(change.type === 'created' && change.mr.author.id !== myself.id)
+			);
+
+			for (const change of interestingChanges) {
+				notify(change);
+			}
+		}
+		// notify({
+		// 	type: 'created',
+		// 	mr: newMrInfos[0],
+		// });
+
+		oldMrInfos = newMrInfos;
 	}
 
 	function getProjectName(mrUrl) {
@@ -66,20 +88,114 @@ javascript:
 			.replace(/[/]/g, ' / ');
 	}
 
-	/*
+	function diff(oldMrInfos, newMrInfos) {
+		const changes = [];
+
+		for (const mr of newMrInfos) {
+			const oldMr = oldMrInfos.find(it => it.href === mr.href);
+
+			if (!oldMr) {
+				changes.push({ type: 'created', mr });
+				continue;
+			}
+
+			if (mr.lastUpdated !== oldMr.lastUpdated) {
+				changes.push({ type: 'updated', mr });
+			}
+
+			if (mr.approved && !oldMr.approved) {
+				changes.push({ type: 'approved', mr });
+			}
+
+			if (!mr.approved && oldMr.approved) {
+				changes.push({ type: 'revoked-approval', mr });
+			}
+
+			if (mr.pipeline.passed && !oldMr.pipeline.passed) {
+				changes.push({ type: 'pipeline-passed', mr });
+			}
+
+			if (mr.comments > oldMr.comments) {
+				changes.push({
+					type: 'new-comments',
+					delta: mr.comments - oldMr.comments,
+					mr
+				});
+			}
+		}
+
+		return changes;
+	}
+
+	function getIcon(name) {
+		return 'https://humodz.github.io/bookmarklets/' + name + '.png';
+	}
+
+	function notify(change) {
+		const mr = change.mr;
+
+		if (change.type === 'created') {
+			new Notification(`New: ${mr.title}`, {
+				icon: mr.author.avatar,
+				body: [
+					`in ${mr.project.name}`,
+					`by ${mr.author.name}`,
+				].join('\n\n'),
+			});
+		} else if (change.type === 'updated') {
+			new Notification(`Updated: ${mr.title}`, {
+				icon: mr.author.avatar,
+				body: [
+					`in ${mr.project.name}`,
+					`by ${mr.author.name}`,
+				].join('\n\n'),
+			});
+		} else if (change.type === 'approved') {
+			new Notification(`Approved: ${mr.title}`, {
+				icon: getIcon('check-mark-button'),
+				body:`in ${mr.project.name}`,
+			});
+		} else if (change.type === 'revoked-approval') {
+			new Notification(`Approval revoked: ${mr.title}`, {
+				icon: getIcon('thumbs-down'),
+				body:`in ${mr.project.name}`,
+			});
+		} else if (change.type === 'pipeline-passed') {
+			new Notification(`CI passed: ${mr.title}`, {
+				icon: getIcon('rocket'),
+				body:`in ${mr.project.name}`,
+			});
+		} else if (change.type === 'new-comments') {
+			new Notification(`${change.delta} new comments: ${mr.title}`, {
+				icon: getIcon('speech-balloon'),
+				body:`in ${mr.project.name}`,
+			});
+		}
+	}
+
+	async function getCurrentUser() {
+		return await fetch('/api/v4/user').then(res => res.json());
+	}
+
 	function getUserAvatar(userId, size = 64) {
 		return `https://gitlab.com/uploads/-/system/user/avatar/${userId}/avatar.png?width=${size}`;
 	}
 
-	function formatMergeRequestInfos(mrsContainer) {
-		return $$('.merge-request', mrsContainer)
-			.map(item => {
-				const titleElem = $('.title a', item);
-				const authorElem = $('.author-link', item);
+	function getMergeRequestInfos() {
+		return $$('#content-body .merge-request')
+			.map(element => {
+				const titleElem = $('.title a', element);
+				const authorElem = $('.author-link', element);
 
 				return {
 					title: titleElem.textContent,
 					href: titleElem.href,
+					approved: !!element.querySelector('[data-testid=approval-solid-icon]'),
+					lastUpdated: element.querySelector('.merge_request_updated_ago')?.textContent,
+					pipeline: {
+						passed: !!element.querySelector('status_success-icon'),
+					},
+					comments: parseInt(element.querySelector('.issuable-comments').textContent.trim()),
 					project: {
 						name: getProjectName(titleElem.href),
 					},
@@ -92,17 +208,6 @@ javascript:
 			});
 	}
 
-	function notifyMergeRequest(mrInfo) {
-		new Notification(mrInfo.title, {
-			icon: mrInfo.author.avatar,
-		});
-	}
-	*/
-
-	function minutes(num) {
-		return num * 60 * 1000;
-	}
-
-	setInterval(updateMergeRequestList, minutes(5));
-	updateMergeRequestList();
+	false && setInterval(updateMergeRequestList, 5 * 60 * 1000);
+	updateMergeRequestList(true);
 })()
